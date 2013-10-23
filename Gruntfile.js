1f4d7ec2838ea8429ab7b68c15801f88c52b0df9
Rhys Evans <rhys.evans@ft.com>
2013-10-23 16:19:13 +0100

    Added demo with largest layout size disabled

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,44 @@
 module.exports = function(grunt) {
   "use strict";
+
+  var demoPageTypes = 'responsive,default,legacy,fluid,resized,disabled'.split(',');
+
+  function constructDemoPagesConfig () {
+    var output = {};
+
+    demoPageTypes.map(function (type) {
+      var conf =  {
+        src: './docs-generator/grid.hbs',
+        engine: "handlebars",
+        dest: './docs/grid-' + type + '.html',
+        variables: {
+          title: type.substr(0, 1).toUpperCase() + type.substr(1) + " grid examples",
+          example: require('./docs-generator/examples.json')
+        }
+      };
+      conf.variables[type] = true;
+      if (!/default|legacy/.test(type)) {
+        conf.variables.isResponsive = true;
+      }
+      output[type] = conf;
+
+    });
+    return output;
+  }
+
+  function constructDemoPagesSassConfig () {
+    var output = {};
+    demoPageTypes.map(function (type) {
+      if (type !== 'legacy') {
+        output['./docs/css/grid-' + type + '.css'] = './src/scss/bundles/' + type + '.scss';
+      }
+      
+    });
+
+    output['./docs/css/docs.css'] = ['./src/scss/docs/docs.scss'];
+    return output;
+  }
+
   
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -28,79 +67,39 @@ module.exports = function(grunt) {
         options: {
           style: 'expanded'
         },
-        files: {
-          './docs/css/grid-default.css': './src/scss/bundles/default.scss',
-          './docs/css/grid-responsive.css': './src/scss/bundles/responsive.scss',
-          './docs/css/grid-fluid.css': './src/scss/bundles/fluid.scss',
-          './docs/css/grid-resized.css': './src/scss/bundles/resized.scss',
-          './docs/css/docs.css': './src/scss/docs/docs.scss'
-        }
+        files: constructDemoPagesSassConfig()
       }
     },
     jshint: {
       files: ['Gruntfile.js'],
       jshintrc: './.jshintrc'
     },
-    template: {
-      responsive: {
-        src: './docs-generator/grid.hbs',
-        engine: "handlebars",
-        dest: './docs/grid-responsive.html',
-        variables: {
-          title: "Responsive grid examples",
-          example: require('./docs-generator/examples.json'),
-          responsive: true
-        }
-      },
-      'default': {
-        src: './docs-generator/grid.hbs',
-        engine: "handlebars",
-        dest: './docs/grid-default.html',
-        variables: {
-          title: "Default grid examples",
-          example: require('./docs-generator/examples.json'),
-          'default': true
-        }
-      },
-      legacy: {
-        src: './docs-generator/grid.hbs',
-        engine: "handlebars",
-        dest: './docs/grid-legacy.html',
-        variables: {
-          title: "Legacy grid examples",
-          example: require('./docs-generator/examples.json'),
-          legacy: true
-        }
-      },
-      fluid: {
-        src: './docs-generator/grid.hbs',
-        engine: "handlebars",
-        dest: './docs/grid-fluid.html',
-        variables: {
-          title: "Fluid grid examples",
-          example: require('./docs-generator/examples.json'),
-          fluid: true
-        }
-      },
-      resized: {
-        src: './docs-generator/grid.hbs',
-        engine: "handlebars",
-        dest: './docs/grid-resized.html',
-        variables: {
-          title: "Resized grid examples",
-          example: require('./docs-generator/examples.json'),
-          resized: true
-        }
-      }
-    },
+    template: constructDemoPagesConfig(),
     markdown: {
       docs: {
         files: [
           {
             expand: true,
-            src: 'README.md',
+            src: './*.md',
             dest: './docs/',
-            rename: function () {
+            ext: '.html'
+          }
+        ],
+        options: {
+          template: './docs-generator/markdown-wrapper.jst',
+          preCompile: function() {},
+          postCompile: function() {},
+          templateContext: {}
+        }
+      },
+      index: {
+        files: [
+          {
+            expand: true,
+            src: './README.md',
+            dest: './docs/',
+            ext: '.html',
+            rename: function (dest, src) {
               return './docs/index.html';
             }
           }
@@ -148,8 +147,12 @@ module.exports = function(grunt) {
     handlebars.registerPartial('examples', grunt.file.read('./docs-generator/partials/examples.hbs', {encoding: 'utf8'}));
     handlebars.registerPartial('head', grunt.file.read('./docs-generator/partials/head.hbs', {encoding: 'utf8'}));
 
-    grunt.task.run(['template', 'markdown:docs', 'prettify:docs', 'sass']);
+    grunt.task.run(['template', 'markdown', 'deleteTmpReadme', 'prettify:docs', 'sass']);
+
     
+  });
+  grunt.registerTask('deleteTmpReadme', function () {
+    grunt.file.delete('./docs/README.html');
   });
 
 };

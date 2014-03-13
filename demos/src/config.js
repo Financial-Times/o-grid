cb6e21b89c421ec21a928bba03847dd5414d07d7
Rhys Evans <rhys.evans@ft.com>
2014-03-13 16:17:19 +0000

    New separate demos for different configurations using origami-build-tools

---
+++ demos/src/config.js
@@ -0,0 +1,25 @@
+var configurations = require('./configurations'),
+    demos = {
+        test: {
+            data: 'demos/src/configurations.js',
+            js: 'demos/src/js/style-switcher.js',
+            template: 'demos/src/test.mustache'
+        }
+    };
+
+Object.keys(configurations).forEach(function (key) {
+    demos[key] = {
+        data: {
+            description: configurations[key]
+        },
+        sass: 'demos/src/scss/' + key + '.scss',
+        template: 'demos/src/layout.mustache'
+    };
+});
+
+module.exports = {
+    options: {
+        scriptMode: 'browserify'
+    },
+    demos: demos
+};
--- grunt-config/clean.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  before: ['./demo', './tmp'],
-  after: ['./tmp']
-};

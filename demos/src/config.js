bb4670b1be3a004ec82a73080915a3a9d8156e5e
Andrew Betts <andrew.betts@ft.com>
2014-10-05 12:15:00 +0100

    Update demos: demo classes should be prefixed with demo-, descriptions belong in origami.json, not in demo itself, there are no comments in demos anymore so no need for .comment

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,26 +1,33 @@
 var configurations = require('./configurations'),
     demos = {
         test: {
+            name: 'test',
             data: 'demos/src/configurations.js',
             js: 'demos/src/js/style-switcher.js',
             sass: 'demos/src/scss/default.scss',
-            template: 'demos/src/test.mustache'
+            template: 'demos/src/test.mustache',
+            expanded: false
         }
     };
 
 Object.keys(configurations).forEach(function (key) {
     demos[key] = {
-        data: {
-            description: configurations[key]
-        },
+        name: key,
+        description: configurations[key],
         sass: 'demos/src/scss/' + key + '.scss',
-        template: 'demos/src/layout.mustache'
+        template: 'demos/src/layout.mustache',
+        expanded: (key == 'default')
     };
 });
+
+var demosArray = [];
+for (var i in demos) {
+    if (demos.hasOwnProperty(i)) demosArray.push(demos[i]);
+};
 
 module.exports = {
     options: {
         scriptMode: 'browserify'
     },
-    demos: demos
+    demos: demosArray
 };

59379849feaf605e5d7dd6d82d27688039a1e358
Alberto Elias <me@alberto-elias.com>
2014-10-20 09:55:07 +0100

    Removed redundant demo config code

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,33 +1,19 @@
 var configurations = require('./configurations'),
-    demos = {
-        test: {
-            name: 'test',
-            data: 'demos/src/configurations.js',
-            js: 'demos/src/js/style-switcher.js',
-            sass: 'demos/src/scss/default.scss',
-            template: 'demos/src/test.mustache',
-            expanded: false
-        }
-    };
+    demos = [];
 
-Object.keys(configurations).forEach(function (key) {
-    demos[key] = {
+Object.keys(configurations).forEach(function(key) {
+    demos.push({
         name: key,
         description: configurations[key],
         sass: 'demos/src/scss/' + key + '.scss',
         template: 'demos/src/layout.mustache',
-        expanded: (key == 'default')
-    };
+        expanded: (key === 'default')
+    });
 });
-
-var demosArray = [];
-for (var i in demos) {
-    if (demos.hasOwnProperty(i)) demosArray.push(demos[i]);
-};
 
 module.exports = {
     options: {
         scriptMode: 'browserify'
     },
-    demos: demosArray
+    demos: demos
 };

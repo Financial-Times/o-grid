1c7e60727ac9d7cb6217da77b182dd9128c041ee
Kaelig <kaelig@deloumeau.fr>
2015-07-02 18:23:02 +0100

    Clean up demos and make layout changes more obvious

--- demos/src/config.js
+++ demos/src/config.js
@@ -8,7 +8,7 @@ Object.keys(configurations).forEach(function(key) {
 		description: configurations[key],
 		sass: 'demos/src/scss/' + key + '.scss',
 		template: 'demos/src/layout.mustache',
-		bodyClasses: 'demo',
+		htmlClasses: 'demo',
 		expanded: (key === 'default')
 	});
 });
@@ -20,8 +20,9 @@ demos.push({
 	template: 'demos/src/test.mustache',
 	data: 'demos/src/configurations.json',
 	js: 'demos/src/js/style-switcher.js',
-	bodyClasses: 'test',
-	expanded: false
+	htmlClasses: 'test',
+	expanded: false,
+	dependencies: ["o-buttons@^2.0.0"]
 });
 
 module.exports = {

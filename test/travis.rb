1dbe5dae82631e2b32daf42fb53aec1904480f12
Kaelig <kaelig@deloumeau.fr>
2015-08-17 18:37:25 +0100

    Fix bugs with configuration and update tests

--- test/travis.rb
+++ test/travis.rb
@@ -15,19 +15,53 @@ FileUtils.mkdir_p "test/output"
 
 # Attempting to add a Layout with a width when gutters are defined
 # in percents (e.g. $o-grid-gutters: 1%;) should throw an error
-stdout, stderr, status = Open3.capture3 "node-sass test/error.scss test/output/error.css --output-style compressed --include-path bower_components"
-puts "Test: adding a layout with columns when gutters are in % should throw an error…"
-raise "Adding a layout this way should fail to compile" if status.success?
-raise "Adding a layout this way should throw a useful error message" unless (stderr.squish.include? "Layouts can only be defined using column widths")
-puts "\e[32mPassed\e[0m"
-stdout, stderr, status = Open3.capture3 "node-sass test/success.scss test/output/success.css --output-style compressed --include-path bower_components"
-puts "Test: adding layouts in multiple ways and show their max-width…"
-raise "Layout (A) should compile when added with a column width" unless File.open("test/output/success.css").read.squish.include? "@media (min-width: 23.125em){A{max-width:800px"
-raise "Layout (B) should compile when added with a layout-width" unless File.open("test/output/success.css").read.squish.include? "@media (min-width: 22.8125em){B{max-width:370px"
-raise "Layout (C) should compile when added with a layout-width and percentage-based gutters" unless File.open("test/output/success.css").read.squish.include? "@media (min-width: 23.25em){C{max-width:800px"
-raise "Layout (Z) should compile when added as a default layout in $o-grid-layouts" unless File.open("test/output/success.css").read.squish.include? "@media (min-width: 75em){Z{max-width:1200px"
+stdout, stderr, status = Open3.capture3 "node-sass test/test.scss test/output/test.css --include-path bower_components"
+
+puts "Test: layouts can be overriden…"
+raise "Defined layout and sizes override the default ones" unless File.open("test/output/test.css").read.squish
+  .include? "Initial layouts and sizes: (X: 800px, Z: 1200px)"
+raise "The list of layout names is updated accordingly" unless File.open("test/output/test.css").read.squish
+  .include? "Initial layout names: X, Z"
 puts "\e[32mPassed\e[0m"
 
-File.delete('test/output/success.css')
+puts "Test: pruning non existant gutter widths, while keeping a default width…"
+raise "Gutter for layout M should have been removed" unless File.open("test/output/test.css").read.squish
+  .include? "Gutter M exists: false"
+raise "There should be a default gutter" unless File.open("test/output/test.css").read.squish
+  .include? "Default gutter exists: true"
+puts "\e[32mPassed\e[0m"
+
+
+puts "Test: adding layouts in multiple ways and ordering them properly…"
+raise "All layouts should have been added in the correct order" unless File.open("test/output/test.css").read.squish
+  .include? "A, B, C, X, Y, Z"
+puts "\e[32mPassed\e[0m"
+
+puts "Test: adding layouts in multiple ways with their max-width and gutter sizes…"
+raise "Layout (A)'s max width should be X's layout width, and inherit the default gutter" unless File.open("test/output/test.css").read.squish
+  .include? "
+  A {
+  	max-width: 800px;
+  	gutter: 10px".squish
+raise "Layout (B) should be added" unless File.open("test/output/test.css").read.squish
+  .include? "B: 365px"
+raise "Gutter for Layout B should be added" unless File.open("test/output/test.css").read.squish
+  .include? "(default: 10px, B: 30px)"
+raise "Layout (B) properties should compile" unless File.open("test/output/test.css").read.squish
+  .include? "
+  @media (min-width: 22.8125em) {
+	  B {
+	    max-width: 800px;
+	    gutter: 30px;".squish
+puts "\e[32mPassed\e[0m"
+
+puts "Test: max-width of the widest layout…"
+raise "Layout (Z) is the widest layout. It should have a max-width equivalent to its defined width" unless File.open("test/output/test.css").read.squish
+  .include? "
+	  Z {
+	    max-width: 1200px;".squish
+puts "\e[32mPassed\e[0m"
+
+File.delete('test/output/test.css')
 
 FileUtils.rmdir "test/output"

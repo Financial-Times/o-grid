8a779ef69377f8e42f0f109f7f41ea96ed306590
Kaelig <kaelig@deloumeau.fr>
2015-08-31 23:30:18 +0100

    Improve testing framework

--- test/travis.rb
+++ test/travis.rb
@@ -83,3 +83,5 @@ describe "oGridColspan" do
     find "Human-friendly colspans: (full-width: 100%, one-half: 50%, one-third: 33.33333%, two-thirds: 66.66667%, one-quarter: 25%, three-quarters: 75%)"
   end
 end
+
+error_summary

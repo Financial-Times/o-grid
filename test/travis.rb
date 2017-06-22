98279a27823884c241c4bb6c3e39e61650ebf0f3
Jake Champion <me@jakechampion.name>
2017-06-22 12:25:07 +0100

    Update travis.rb

--- test/travis.rb
+++ test/travis.rb
@@ -5,12 +5,12 @@ require_relative 'test-framework'
 
 describe "Overriding layouts" do
   it "should replace the default layouts with the new ones" do
-    find "Initial layouts and sizes: (X: 800px, Z: 1200px)"
+    find "Initial layouts and sizes: (x: 800px, z: 1200px)"
   end
   it "should update the list of layout names" do
-    find "Initial layout names: X, Z"
+    find "Initial layout names: x, z"
   end
-  it "should remove gutter for layout M" do
+  it "should remove gutter for layout m" do
     find "Gutter M exists: false"
   end
   it "should always keep a default gutter width" do
@@ -20,52 +20,52 @@ end
 
 describe "Adding layouts in different ways" do
   it "should order them from the narrowest to the widest" do
-    find "Layout names: A, B, C, X, Y, Z" and
-    find "Layouts: (A: 340px, B: 365px, C: 372px, X: 800px, Y: 1100px, Z: 1200px)"
+    find "Layout names: a, b, c, x, y, z" and
+    find "Layouts: (a: 340px, b: 365px, c: 372px, x: 800px, y: 1100px, z: 1200px)"
   end
   it "should order gutters from the narrowest to the widest layout" do
-    find "Gutters: (default: 10px, Y: 20px)" and
-    find "Gutters: (default: 10px, B: 30px, Y: 20px)"
+    find "Gutters: (default: 10px, y: 20px)" and
+    find "Gutters: (default: 10px, b: 30px, y: 20px)"
   end
 end
 
 describe "Adding a layout *without* a gutter" do
   it "should add that layout" do
-    find "Layout names: A,"
+    find "Layout names: a,"
   end
   it "should make that layout inherit from the previous' layout gutter" do
-    find "Gutter A: 10px"
+    find "Gutter a: 10px"
   end
 end
 
 describe "Adding a layout *with* a gutter" do
   it "should be assigned this gutter" do
-    find "Gutter Y: 20px"
+    find "Gutter y: 20px"
   end
   it "should impact wider layouts's gutters" do
-    find "Gutter Z: 20px"
+    find "Gutter z: 20px"
   end
   it "should not change narrower layouts's gutters" do
-    find "Gutter A in Y: 10px"
+    find "Gutter a in y: 10px"
   end
 end
 
 describe "Maximum width of a layout" do
   it "should be equivalent to the following layout's width" do
-    find "A: 340px, X: 800px" and find "Max-width A: 800px"
+    find "a: 340px, x: 800px" and find "Max-width a: 800px"
   end
   it "should be its own width for the widest layout" do
-    find "Z: 1200px" and find "Max-width Z: 1200px"
+    find "z: 1200px" and find "Max-width z: 1200px"
   end
 end
 
 describe "Resetting gutters to only a default" do
   it "should give all layouts the same gutter" do
-    find "Gutter A after reset: 20px" and
-    find "Gutter B after reset: 20px" and
-    find "Gutter C after reset: 20px" and
-    find "Gutter X after reset: 20px" and
-    find "Gutter Y after reset: 20px"
+    find "Gutter a after reset: 20px" and
+    find "Gutter b after reset: 20px" and
+    find "Gutter c after reset: 20px" and
+    find "Gutter x after reset: 20px" and
+    find "Gutter y after reset: 20px"
   end
 end
 
